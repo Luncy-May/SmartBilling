644267e739f28fa58667e7142c42e28eda3e2490
Luncy-May <cheng.li1@wustl.edu>
2024-10-08 15:49:13 -0500

    init further

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,33 +1,29 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Navbar } from './components'
+import { Routes, Route } from 'react-router-dom';
+import { Community, About, Calendar, Dashboard, Home, MyPlans, Profile, Setting, Support, Team } from './pages';
+import { Login, Register, Delete } from './pages/AuthPages';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about/*" element={<About />} />
+        <Route path="/calendar/*" element={<Calendar />} />
+        <Route path="/dashboard/*" element={<Dashboard />} />
+        <Route path="/myplans/*" element={<MyPlans />} />
+        <Route path="/profile/*" element={<Profile />} />
+        <Route path="/setting/*" element={<Setting />} />
+        <Route path="/support/*" element={<Support />} />
+        <Route path="/team/*" element={<Team />} />
+        <Route path="/login/*" element={<Login />} />
+        <Route path="/register/*" element={<Register />} />
+        <Route path="/community/*" element={<Community />} />
+      </Routes>
     </>
   )
 }
---
+++ client/src/components/ExpenseCategories.jsx
@@ -0,0 +1,106 @@
+const ExpenseCategories = {
+    "Housing": {
+        "Mortgage or rent": 0,
+        "Property taxes": 0,
+        "Household repairs": 0,
+        "HOA fees": 0
+    },
+    "Transportation": {
+        "Car payment": 0,
+        "Car warranty": 0,
+        "Gas": 0,
+        "Tires": 0,
+        "Maintenance and oil changes": 0,
+        "Parking fees": 0,
+        "Repairs": 0,
+        "Registration and DMV Fees": 0
+    },
+    "Food": {
+        "Groceries": 0,
+        "Restaurants": 0,
+        "Pet food": 0
+    },
+    "Utilities": {
+        "Electricity": 0,
+        "Water": 0,
+        "Garbage": 0,
+        "Phones": 0,
+        "Cable": 0,
+        "Internet": 0
+    },
+    "Clothing": {
+        "Adults\u2019 clothing": 0,
+        "Adults\u2019 shoes": 0,
+        "Children\u2019s clothing": 0,
+        "Children\u2019s shoes": 0
+    },
+    "Medical/Healthcare": {
+        "Primary care": 0,
+        "Dental care": 0,
+        "Specialty care": 0,
+        "Urgent care": 0,
+        "Medications": 0,
+        "Medical devices": 0
+    },
+    "Insurance": {
+        "Health insurance": 0,
+        "Homeowner\u2019s or renter\u2019s insurance": 0,
+        "Home warranty or protection plan": 0,
+        "Auto insurance": 0,
+        "Life insurance": 0,
+        "Disability insurance": 0
+    },
+    "Household Items/Supplies": {
+        "Toiletries": 0,
+        "Laundry detergent": 0,
+        "Dishwasher detergent": 0,
+        "Cleaning supplies": 0,
+        "Tools": 0
+    },
+    "Personal": {
+        "Gym memberships": 0,
+        "Haircuts": 0,
+        "Salon services": 0,
+        "Cosmetics": 0,
+        "Babysitter": 0,
+        "Subscriptions": 0
+    },
+    "Debt": {
+        "Personal loans": 0,
+        "Student loans": 0,
+        "Credit cards": 0
+    },
+    "Retirement": {
+        "Financial planning": 0,
+        "Investing": 0
+    },
+    "Education": {
+        "Children\u2019s college": 0,
+        "Your college": 0,
+        "School supplies": 0,
+        "Books": 0
+    },
+    "Savings": {
+        "Emergency fund": 0,
+        "Big purchases": 0,
+        "Other savings": 0
+    },
+    "Gifts/Donations": {
+        "Birthday": 0,
+        "Anniversary": 0,
+        "Wedding": 0,
+        "Christmas": 0,
+        "Special occasion": 0,
+        "Charities": 0
+    },
+    "Entertainment": {
+        "Alcohol and/or bars": 0,
+        "Games": 0,
+        "Movies": 0,
+        "Concerts": 0,
+        "Vacations": 0,
+        "Subscriptions": 0
+    }
+}
+
+export default ExpenseCategories
